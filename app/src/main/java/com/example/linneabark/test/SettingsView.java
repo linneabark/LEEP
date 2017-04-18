d4a15055aa7da6334fab39908627c3d4d83ffe99
linneabark <bark_linnea@hotmail.com>
2017-04-18 13:49:35 +0200

    Lagt till vyer

---
+++ app/src/main/java/com/example/linneabark/test/SettingsView.java
@@ -0,0 +1,31 @@
+package com.example.linneabark.test;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Eli on 2017-04-06.
+ */
+
+public class SettingsView extends View {
+    public SettingsView(Context context) {
+        super(context);
+        init(null,0);
+    }
+
+    public SettingsView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public SettingsView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs, defStyleAttr);
+    }
+
+    private void init(AttributeSet attrs, int defStyleAttr){
+
+    }
+}
---
+++ app/src/main/java/com/example/linneabark/test/TimeLogController.java
@@ -0,0 +1,8 @@
+package com.example.linneabark.test;
+
+/**
+ * Created by Eli on 2017-04-14.
+ */
+
+public class TimeLogController {
+}
