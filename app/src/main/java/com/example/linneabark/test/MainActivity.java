c96661448a8216c46d00ecef8414332afdac1448
Rudolf <eli.knoph@gmail.com>
2017-05-06 17:09:04 +0200

    Defined a toolbar instead of just a menu

--- app/src/main/java/com/example/linneabark/test/MainActivity.java
+++ app/src/main/java/com/example/linneabark/test/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -35,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         Button registerButton = (Button) this.findViewById(R.id.registerButton);
         Button loginButton = (Button) this.findViewById(R.id.loginButton);
         Button register = (Button) this.findViewById(R.id.register);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         /*loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
