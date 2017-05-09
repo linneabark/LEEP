b8ae63a8d664476645cc891d0bbcae3a5865b885
linneabark <bark_linnea@hotmail.com>
2017-05-09 11:56:11 +0200

    Cleand code

--- app/src/main/java/com/example/linneabark/test/PopUpWindow.java
+++ app/src/main/java/com/example/linneabark/test/PopUpWindow.java
@@ -1,8 +1,19 @@
 package com.example.linneabark.test;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by linneabark on 2017-05-09.
  */
 
-public class PopUpWindow {
+public class PopUpWindow extends Activity {
+
+    boolean isClicked = true;
+    PopUpWindow popUpWindow;
+
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
 }
