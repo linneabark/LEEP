a0b0c9d5ceb6aa37d1a4457291b7790dc4d94c9e
linneabark <bark_linnea@hotmail.com>
2017-05-09 10:01:41 +0200

    Changed the design in list_group and list_item

--- app/src/main/java/com/example/linneabark/test/ExpandableListAdapter.java
+++ app/src/main/java/com/example/linneabark/test/ExpandableListAdapter.java
@@ -77,8 +77,6 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         lblListHeader.setTypeface(null, Typeface.BOLD);
         lblListHeader.setText(headerTitle);
 
-
-
         return convertView;
     }
 
