39a46c0afe43a3e1c227f76e5c748da4aaffbf84
linneabark <bark_linnea@hotmail.com>
2017-05-25 14:24:48 +0200

    Added SettingsControllerTest class

---
+++ app/src/test/java/edu/chl/leep/ctrl/SettingsControllerTest.java
@@ -0,0 +1,26 @@
+package edu.chl.leep.ctrl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by linneabark on 2017-05-25.
+ */
+public class SettingsControllerTest {
+    @Test
+    public void onCreateView() throws Exception {
+
+    }
+
+    @Test
+    public void choosePopUp() throws Exception {
+
+    }
+
+    @Test
+    public void getExpanded() throws Exception {
+
+    }
+
+}
