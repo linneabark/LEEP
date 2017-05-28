7ba4fe9d742df8fd6fc49696b4c39584a80f7301
linneabark <bark_linnea@hotmail.com>
2017-05-28 11:31:29 +0200

    Added method

---
+++ app/src/main/java/com/example/linneabark/test/ShowCategoryPopUp.java
@@ -0,0 +1,11 @@
+package com.example.linneabark.test;
+
+/**
+ * Created by linneabark on 2017-05-28.
+ */
+
+public abstract class ShowCategoryPopUp {
+
+    public abstract void showCategoryPopUp();
+
+}
