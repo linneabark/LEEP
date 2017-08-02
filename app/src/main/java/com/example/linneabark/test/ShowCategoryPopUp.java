f13309a42ab73bbfa47439ff191c351849ac0270
linneabark <bark_linnea@hotmail.com>
2017-08-02 17:11:17 +0200

    bla

--- app/src/main/java/com/example/linneabark/test/ShowCategoryPopUp.java
+++ app/src/main/java/com/example/linneabark/test/ShowCategoryPopUp.java
@@ -42,6 +42,7 @@ public class ShowCategoryPopUp {
             public void onClick(View v) {
                 setCategory(context, buttonTag);
                 helpDialog.dismiss();
+
             }
         });
 
